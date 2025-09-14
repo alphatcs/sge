cde3f50d0ce180a0cc79132c131eedad14ded0a3
alphatcs <adm@alphatcs.com.br>
2025-09-14 15:07:43 -0300

    inserido espacos nos comentarios 2

--- app/settings.py
+++ app/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    },
-#}
+# }
 
 DATABASES = {
     'default': {
@@ -110,8 +110,8 @@ DATABASES = {
     }
 }
 
-#database_url = os.getenv('DATABASE_URL')
-#DATABASES['default'] = dj_database_url.parse('database_url')
+# database_url = os.getenv('DATABASE_URL')
+# DATABASES['default'] = dj_database_url.parse('database_url')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
